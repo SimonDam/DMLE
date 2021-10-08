cfbfa7086e668cb0779168b33a711ac1240651c1
Simon <simon9400@live.dk>
2021-10-08 23:51:39 +0200

    EOD commit

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DatabaseService } from './database.service';
 
 @Component({
     selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ClientApp';
+    title = 'ClientApp';
+    id : string = "";
+    name : string = "";
+
+    constructor(private databaseService: DatabaseService) {
+
+    }
+
+    getValue(input : string) : void {
+        this.id = input;
+    }
+
+    LoadProfile() : void{
+        let id_int : number = Number(this.id);
+        if(isNaN(id_int)) {
+            this.name = "Only number is allowed.";
+        }
+        else {
+            this.name = this.databaseService.getProfile(id_int);
+        }
+    }
 }
