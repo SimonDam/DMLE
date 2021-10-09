5b825cc6de4c364cc7a320eaf86931ec825bbd98
Simon <simon9400@live.dk>
2021-10-09 13:22:48 +0200

    Added Spring backend

---
+++ src/main/java/com/backend/DMLE/DmleApplication.java
@@ -0,0 +1,13 @@
+package com.backend.DMLE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DmleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DmleApplication.class, args);
+	}
+
+}
